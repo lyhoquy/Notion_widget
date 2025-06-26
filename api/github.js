8cc8d66c51fcf89a3f1f34ccbe39e69fb7256415
lyhoquy <lyhoquy20072004@gmail.com>
2025-06-26 21:24:37 +0700

    feat

--- api/github.js
+++ api/github.js
@@ -1,9 +1,13 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
   const username = req.query.username;
 
   if (!username) {
-    return res.status(400).json({ error: "Thiếu username" });
+    return res.status(400).json({ error: "Missing username" });
   }
 
   const query = `
